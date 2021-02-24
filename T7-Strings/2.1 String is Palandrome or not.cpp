28db432dd2d3af9a57172ee10c53c373680939c2
Saumyanshu <saumyasanjay08@gmail.com>
2021-02-24 16:21:50 +0530

    quee

---
+++ T7-Strings/2.1 String is Palandrome or not.cpp
@@ -0,0 +1,23 @@
+//String is palindrome or not
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	int n= s.length();
+	int l=0,h=n-1,f=1;
+	while(l<h)
+	{
+		if(s[l]!=s[h])
+		 f=0;
+		 l++;
+		 h--;
+	}
+	if(f)
+	cout<<"Yes";
+	else
+	cout<<"No";
+}
+
