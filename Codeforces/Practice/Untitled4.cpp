b2ca183e7504c83b11200f3a3a09c7901758fbd0
Saumyanshu <saumyasanjay08@gmail.com>
2021-04-01 20:57:24 +0530

    stl

---
+++ Codeforces/Practice/Untitled4.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+	ll t;
+	cin>>t;
+	vector<int>arr;
+	ll a;
+	
+	for(int i=0;i<t;i++)
+	{
+		cin>>a;
+		arr.push_back(a);
+	}
+	
+	
+}
