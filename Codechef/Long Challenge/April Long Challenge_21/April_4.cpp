4f7b558dac2129084d618953e9d4a0e6c0a79465
Saumyanshu <saumyasanjay08@gmail.com>
2021-04-05 22:20:14 +0530

    strings

---
+++ Codechef/Long Challenge/April Long Challenge_21/April_4.cpp
@@ -0,0 +1,15 @@
+#define ll long long
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll k = (n-1)*14;
+		cout<<(k+20)<<endl;
+	}
+}
---
+++ T7-Strings/7.1 Leftmost Repeating Character(effiecient 1).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+const int CHAR=256;
+int leftMost(string &str) 
+{
+    int fIndex[CHAR]={0};
+    fill(fIndex,fIndex+CHAR,-1);
+    int res=INT_MAX;
+    for(int i=0;i<str.length();i++){
+        int fi=fIndex[str[i]];
+        if(fi==-1)
+        fIndex[str[i]]=i;
+        else
+        res=min(res,fi);
+    }
+    
+    return (res==INT_MAX)?-1:res;
+}
+ 
+int main() 
+{ 
+    string str ;
+    cin>>str;
+    cout<<leftMost(str)<<endl;  
+    
+    return 0; 
+} 
